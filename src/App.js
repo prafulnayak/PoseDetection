4b512e6d08ac91d893e79450499a04e50351859e
praful nayak <prafulnayak1988@gmail.com>
2024-09-22 19:08:01 +0530

    pose detection2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
 // 7. Drawing utilities from tensorflow DONE
 // 8. Draw functions DONE
 
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
 import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
@@ -18,51 +18,46 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  //  Load posenet
-  const runPosenet = async () => {
-    const net = await posenet.load({
-      inputResolution: { width: 640, height: 480 },
-      scale: 0.8,
-    });
-    //
-    setInterval(() => {
-      detect(net);
-    }, 100);
-  };
+  useEffect(() => {
+    const runPoseNet = async () => {
+      await tf.setBackend("webgl");
+      const net = await posenet.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        inputResolution: { width: 320, height: 240 },
+        multiplier: 0.75
+      });
 
-  const detect = async (net) => {
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+      const detect = async () => {
+        if (
+          webcamRef.current &&
+          webcamRef.current.video.readyState === 4
+        ) {
+          const video = webcamRef.current.video;
+          const videoWidth = video.videoWidth;
+          const videoHeight = video.videoHeight;
 
-      // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+          webcamRef.current.video.width = videoWidth;
+          webcamRef.current.video.height = videoHeight;
 
-      // Make Detections
-      const pose = await net.estimateSinglePose(video);
-      console.log(pose);
+          const pose = await net.estimateSinglePose(video, {
+            flipHorizontal: false,
+          });
 
-      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
-    }
-  };
+          const ctx = canvasRef.current.getContext("2d");
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
 
-  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
-    const ctx = canvas.current.getContext("2d");
-    canvas.current.width = videoWidth;
-    canvas.current.height = videoHeight;
+          drawKeypoints(pose.keypoints, 0.6, ctx);
+          drawSkeleton(pose.keypoints, 0.6, ctx);
+        }
+      };
 
-    drawKeypoints(pose["keypoints"], 0.6, ctx);
-    drawSkeleton(pose["keypoints"], 0.7, ctx);
-  };
+      setInterval(detect, 100); // Throttle frame processing
+    };
 
-  runPosenet();
+    runPoseNet();
+  }, []);
 
   return (
     <div className="App">
@@ -77,11 +72,10 @@ function App() {
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 640,
-            height: 480,
+            width: 320,
+            height: 240,
           }}
         />
-
         <canvas
           ref={canvasRef}
           style={{
@@ -92,8 +86,8 @@ function App() {
             right: 0,
             textAlign: "center",
             zindex: 9,
-            width: 640,
-            height: 480,
+            width: 320,
+            height: 240,
           }}
         />
       </header>
